d857b9395137b87fc07b3eccc40bf7165c39fb0e
[SJ] <soorajsunil9495@gmail.com>
2024-10-15 23:21:34 +0530

    18th commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,41 @@
-import React from 'react'
-import Navbar from '../src/components/Navbar'
-import Homepage from './components/Homepage'
-import AdvancedFooter from './pages/AdvancedFooter'
-import ContactPage from './pages/ContactPage'
+import React from 'react';
+import Navbar from '../src/components/Navbar';
+import Homepage from './components/Homepage';
+import AdvancedFooter from './pages/AdvancedFooter';
+import ContactPage from './pages/ContactPage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AboutEessan1 from './pages/AboutEessan1'
+import AboutEessan1 from './pages/AboutEessan1';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import VisionAndMission from './pages/VisionAndMission';
+import WhyEssan1 from './pages/WhyEssan1';
+import Contact from './pages/Contact';
+import Product from './pages/Product';
 
 function App() {
   return (
     <>
       <Router>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Homepage/>} />
-          <Route path="/about" element={<AboutEessan1/>} />
-          <Route path="/contact" element={<ContactPage/>} />
+          <Route 
+            path="/" 
+            element={
+              <>
+                <Homepage />
+                <WhyEssan1 />
+                <VisionAndMission />
+                <Product />
+                <Contact />
+              </>
+            } 
+          />
+          <Route path="/about" element={<AboutEessan1 />} />
+          <Route path="/contact" element={<ContactPage />} />
         </Routes>
-        <AdvancedFooter/>
+        <AdvancedFooter />
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -5,10 +5,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFire } from '@fortawesome/free-solid-svg-icons';
 import { faHandHoldingDroplet, faUsers } from '@fortawesome/free-solid-svg-icons';
 import '../assets/font.css';
-import VisionAndMission from '../pages/VisionAndMission';
-import WhyEssan1 from '../pages/WhyEssan1';
-import Contact from '../pages/Contact';
-import Product from '../pages/Product';
 
 const bg_1 = "https://lh3.googleusercontent.com/pw/AP1GczOeS1R8veCFTLgHW0f_FwDwgywb6YTWFtgsLo__6SM6-yNKFxDwL3j3z1rovlxz_WLUckuZvmkxkcxvnXHXbqBY855QyXS2e_AuKd5FpAnx_AOfnyqO87YjByTO6G6n-ZJi9NvKvCii0YfYg0zRPtFc=w1705-h945-s-no-gm?authuser=0";
 
@@ -27,57 +23,51 @@ const Homepage = () => {
             <a href="tel:+919042032363"><button className='btng poppins-regular'>Start Today</button></a>
 
 
+
             <div className="cardss">
-          <div className="container content-section">
-            <div className="row">
-              <div className="col-md-4">
-                <div className="card text-center p-4 icon-card">
-                  <div className="icon-wrapper">
-                    <div className="bg-icon"><FontAwesomeIcon className="hm-icons" icon={faFire} /></div>
+              <div className="container content-section">
+                <div className="row">
+                  <div className="col-md-4">
+                    <div className="card text-center p-4 icon-card">
+                      <div className="icon-wrapper">
+                        <div className="bg-icon"><FontAwesomeIcon className="hm-icons" icon={faFire} /></div>
+                      </div>
+                      <div className="card-body">
+                        <h5 className="card-title">Energy</h5>
+                        <p className="card-text">Affordable and clean energy</p>
+                      </div>
+                    </div>
                   </div>
-                  <div className="card-body">
-                    <h5 className="card-title">Energy</h5>
-                    <p className="card-text">Affordable and clean energy</p>
-                  </div>
-                </div>
-              </div>
 
-              <div className="col-md-4">
-                <div className="card text-center p-4 icon-card">
-                  <div className="icon-wrapper">
-                    <div className="bg-icon"><FontAwesomeIcon className="hm-icons" icon={faHandHoldingDroplet} /></div>
+                  <div className="col-md-4">
+                    <div className="card text-center p-4 icon-card">
+                      <div className="icon-wrapper">
+                        <div className="bg-icon"><FontAwesomeIcon className="hm-icons" icon={faHandHoldingDroplet} /></div>
+                      </div>
+                      <div className="card-body">
+                        <h5 className="card-title">Water</h5>
+                        <p className="card-text">Clean water and sanitation</p>
+                      </div>
+                    </div>
                   </div>
-                  <div className="card-body">
-                    <h5 className="card-title">Water</h5>
-                    <p className="card-text">Clean water and sanitation</p>
-                  </div>
-                </div>
-              </div>
 
-              <div className="col-md-4">
-                <div className="card text-center p-4 icon-card">
-                  <div className="icon-wrapper">
-                    <div className="bg-icon"><FontAwesomeIcon className="hm-icons" icon={faUsers} /></div>
-                  </div>
-                  <div className="card-body">
-                    <h5 className="card-title">Community</h5>
-                    <p className="card-text">Sustainable cities and communities</p>
+                  <div className="col-md-4">
+                    <div className="card text-center p-4 icon-card">
+                      <div className="icon-wrapper">
+                        <div className="bg-icon"><FontAwesomeIcon className="hm-icons" icon={faUsers} /></div>
+                      </div>
+                      <div className="card-body">
+                        <h5 className="card-title">Community</h5>
+                        <p className="card-text">Sustainable cities and communities</p>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-          </div>
-        </div>
-
-        
       </div>
-
-      <WhyEssan1 />
-      <VisionAndMission />
-      <Product />
-      <Contact />
     </>
   );
 };
