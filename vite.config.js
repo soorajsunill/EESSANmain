e736f3c8df2ec81e7c89bd7be1d0f7693afbfa78
[SJ] <soorajsunil9495@gmail.com>
2024-10-15 14:21:53 +0530

    4rd commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['bootstrap']
+      external: ['bootstrap/dist/css/bootstrap.min.css']
     }
   }
 });
